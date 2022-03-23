9936f8d22db8a35aba62f04186d5973736a3ab3e
thasup <thanachonfirst@hotmail.com>
2022-03-23 16:59:46 +0700

    Feat: add typing effect on Hero panel

--- components/HeroPanel.js
+++ components/HeroPanel.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 const HeroPanel = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Full-stack Developer",
+        "Front-End Developer",
+        "Self-taught Programmer",
+        "Mechanical Engineer",
+      ],
+      loop: true,
+      startDelay: 0,
+      typeSpeed: 80,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div
       id="hero"
@@ -8,10 +32,7 @@ const HeroPanel = () => {
     >
       <h1 className="hero-title text-center">Hi! I am Thanachon</h1>
       <p className="hero-subtitle text-center">
-        <span
-          className="typed"
-          data-typed-items="Full-stack Developer, Front-End Developer, Self-taught Programmer, Mechanical Engineer"
-        ></span>
+        <span ref={el}></span>
       </p>
       <a className="btn-started">
         <i className="fas fa-chevron-down"></i>
