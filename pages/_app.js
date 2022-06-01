d08e3ae6ebbd99c4c8f683d81a8fbfb574d92ea3
thasup <thanachonfirst@hotmail.com>
2022-06-02 00:50:03 +0700

    Refactor css to scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,25 @@
-import React from "react";
-import Head from "next/head";
-import { Container, SSRProvider } from "react-bootstrap";
+import React from 'react';
+import Head from 'next/head';
+import { Container, SSRProvider } from 'react-bootstrap';
 
-import "../styles/bootstrap.css";
-import "../styles/globals.css";
+import '../styles/bootstrap.css';
+import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => {
-  return (
-    <SSRProvider>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      {/* <CustomHeader currentUser={currentUser} {...pageProps} /> */}
-      <main className="pb-5">
-        <Container fluid className="px-0">
-          <Component {...pageProps} />
-        </Container>
-      </main>
-      {/* <Footer /> */}
-    </SSRProvider>
-  );
+	return (
+		<SSRProvider>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			{/* <CustomHeader currentUser={currentUser} {...pageProps} /> */}
+			<main className="pb-5">
+				<Container fluid className="px-0">
+					<Component {...pageProps} />
+				</Container>
+			</main>
+			{/* <Footer /> */}
+		</SSRProvider>
+	);
 };
 
 export default MyApp;
