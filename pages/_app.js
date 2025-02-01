f39a74f5419afd4475e812633b0afc8113aafa9b
thasup <thanachonfirst@hotmail.com>
2025-02-01 17:54:12 +0700

    chore: Add Google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import { Container, SSRProvider } from 'react-bootstrap';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import '../styles/bootstrap.css';
 import '../styles/globals.scss';
@@ -32,6 +33,7 @@ const MyApp = ({ Component, pageProps }) => {
 				<meta name="twitter:image" content="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/thanachon.me/I'm%20Thanachon%20%7C%20Portfolio/Explore%20Thanachon's%20portfolio%2C%20a%20vibrant%20showcase%20of%20creativity%2C%20innovation%2C%20and%20technical%20skills./https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F58c47497-c82b-46be-a4aa-7ce92d32f0d4.jpg%3Ftoken%3DvQjX8-eyT3e5s_Rsd8CfvKujFfw-j1O2d2M8SNwdMew%26height%3D800%26width%3D1200%26expires%3D33258829395/og.png" />
 			</Head>
 			<main className="pb-5">
+				<GoogleAnalytics trackPageViews />
 				<Container fluid className="px-0">
 					<Component {...pageProps} />
 				</Container>
