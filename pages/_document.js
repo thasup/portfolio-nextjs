a1cf1e50a968748478913ac3aeea6a15c631988b
thasup <thanachonfirst@hotmail.com>
2025-07-07 14:56:16 +0700

    feat: Update Google Tag Manager integration to use environment variable for measurement ID

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
 							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','GTM-54V2W36');
+							})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_MEASUREMENT_ID}');
 						`,
 					}}
 				/>
@@ -59,7 +59,7 @@ export default function Document() {
       <body>
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-54V2W36`}
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_MEASUREMENT_ID}`}
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
